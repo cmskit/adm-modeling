94f65d8369b6213bfa04d107a5ffb470f31ef7f5
chris <taubmann@more-elements.com>
2015-01-09 17:48:53 +0100

    fixes

--- json_io.php
+++ json_io.php
@@ -3,33 +3,29 @@
 require dirname(dirname(__DIR__)) . '/inc/php/session.php';
 
 $projectName = preg_replace('/\W/', '', $_GET['project']);
-if(!isset($_SESSION[$projectName]['root'])) exit('no Rights to edit!');
+if (!isset($_SESSION[$projectName]['root'])) exit('no Rights to edit!');
 $draft_path = '../../../projects/' . $projectName . '/objects/__draft.php';
 
-if (isset($_POST['json']))
-{
-// create a backup for export
-if ($_POST['backup'] == 1)
-{
-	copy($draft_path, substr($draft_path,0,-3).'bac.php');
-}
+if (isset($_POST['json'])) {
+    // create a backup for export
+    if ($_POST['backup'] == 1) {
+        @copy($draft_path, substr($draft_path, 0, -3) . 'bac.php');
+    }
 
-$str = 
-'<?php
+    $str =
+        '<?php
 $model = <<<\'EOD\'
-'.trim(str_replace('\\"','"',$_POST['json'])).'
+' . trim(str_replace('\\"', '"', $_POST['json'])) . '
 EOD;
 ?>
 ';
-	file_put_contents($draft_path, $str);
-	@chmod($draft_path, 0777);
-	echo 'saved';
-}
-else
-{
-	$model = '{"object":[]}';
-	@include $draft_path;
-	header ("Content-Type:text/json");
-	echo $model;
+    file_put_contents($draft_path, $str);
+    @chmod($draft_path, 0777);
+    echo 'saved';
+} else {
+    $model = '{"object":[]}';
+    @include $draft_path;
+    header("Content-Type:text/json");
+    echo $model;
 }
 ?>
